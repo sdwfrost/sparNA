cba99f60981078723a2c3b0aacf4275f7299f71f
Bede Constantinides <bedeabc@gmail.com>
2015-07-07 19:31:23 +0100

    Renames tests

--- tests/tests.sh
+++ tests/tests.sh
@@ -3,19 +3,19 @@
 source ../../venv/bin/activate
 
 # Fast agnostic HCV multisample
-# ../pipeline.py --fwd-reads-sig _F --rev-reads-sig _R --norm-k-list 31 --norm-cov-list 1,2,5,10 --asm-k-list 21,33,55,77 --reads-dir data_small --threads 12 --est-ref-len 9678
+# ../sparna.py --fwd-reads-sig _F --rev-reads-sig _R --norm-k-list 31 --norm-cov-list 1,2,5,10 --asm-k-list 21,33,55,77 --reads-dir data_small --threads 12 --est-ref-len 9678
 
 # Fast agnostic HCV multisample with mapping
-# ../pipeline.py --fwd-reads-sig _F --rev-reads-sig _R --norm-k-list 31 --norm-cov-list 1,2,5,10 --asm-k-list 21,33,55,77 --reads-dir data_small --threads 12 --reference /Users/Bede/Research/Datasets/ref_sequences/hcv/h77.fasta
+# ../sparna.py --fwd-reads-sig _F --rev-reads-sig _R --norm-k-list 31 --norm-cov-list 1,2,5,10 --asm-k-list 21,33,55,77 --reads-dir data_small --threads 12 --reference /Users/Bede/Research/Datasets/ref_sequences/hcv/h77.fasta
 
 # Thorough agnostic HCV multisample
-# ../pipeline.py --fwd-reads-sig _F --rev-reads-sig _R --norm-k-list 31 --norm-cov-list 1,2,5,10,20 --asm-k-list 21,33,55,77 --reads-dir data --threads 12 --reference /Users/Bede/Research/Datasets/ref_sequences/hcv/h77.fasta
+# ../sparna.py --fwd-reads-sig _F --rev-reads-sig _R --norm-k-list 31 --norm-cov-list 1,2,5,10,20 --asm-k-list 21,33,55,77 --reads-dir data --threads 12 --reference /Users/Bede/Research/Datasets/ref_sequences/hcv/h77.fasta
 
 # Gold standard agnostic HCV multisample
-../pipeline.py --fwd-reads-sig _F --rev-reads-sig _R --norm-k-list 19,25,31 --norm-cov-list 1,2,5,10,20 --asm-k-list 21,33,55,77 --reads-dir data --threads 12 --reference /Users/Bede/Research/Datasets/ref_sequences/hcv/h77.fasta
+../sparna.py --fwd-reads-sig _F --rev-reads-sig _R --norm-k-list 19,25,31 --norm-cov-list 1,2,5,10,20 --asm-k-list 21,33,55,77 --reads-dir data --threads 12 --reference /Users/Bede/Research/Datasets/ref_sequences/hcv/h77.fasta
 
 # Fast agnostic HCV single sample
-# ../pipeline.py --fwd-reads /Users/Bede/Research/Analyses/phe_asm/phe_hcv_pipeline/tests/data/hcv_n1000_F.fastq --rev-reads /Users/Bede/Research/Analyses/phe_asm/phe_hcv_pipeline/tests/data/hcv_n1000_R.fastq --fwd-reads-sig _F --rev-reads-sig _R --norm-k-list 25,31 --norm-c-list 5,10,15,20 --asm-k-list 33,43  --threads 12
+# ../sparna.py --fwd-reads /Users/Bede/Research/Analyses/phe_asm/phe_hcv_pipeline/tests/data/hcv_n1000_F.fastq --rev-reads /Users/Bede/Research/Analyses/phe_asm/phe_hcv_pipeline/tests/data/hcv_n1000_R.fastq --fwd-reads-sig _F --rev-reads-sig _R --norm-k-list 25,31 --norm-c-list 5,10,15,20 --asm-k-list 33,43  --threads 12
 
 # --reference --asm-using-ref
 
